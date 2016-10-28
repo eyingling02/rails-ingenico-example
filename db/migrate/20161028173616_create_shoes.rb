804c9112ff65218a1b21d0f56e35dd480af15472
eyingling02 <eyingling02@gmail.com>
2016-10-28 10:44:32 -0700

    Add setup files

---
+++ db/migrate/20161028173616_create_shoes.rb
@@ -0,0 +1,10 @@
+class CreateShoes < ActiveRecord::Migration[5.0]
+  def change
+    create_table :shoes do |t|
+      t.string :name
+      t.float :price
+
+      t.timestamps
+    end
+  end
+end
